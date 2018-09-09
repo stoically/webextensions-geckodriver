944299f39b9683d270942c86634fc047a7f26f49
Colin Caine <cmc@cs.man.ac.uk>
2018-09-09 18:45:13 +0100

    Simplify geckodriver.js with await

--- src/geckodriver.js
+++ src/geckodriver.js
@@ -12,7 +12,6 @@ const firefox = require('selenium-webdriver/firefox');
 const webdriver = require('selenium-webdriver');
 const FxRunnerUtils = require('fx-runner/lib/utils');
 const Fs = require('mz/fs');
-const Context = firefox.Context;
 
 // Note: Geckodriver already has quite a good set of default preferences
 // for disabling various items.
@@ -39,64 +38,64 @@ function promiseActualBinary(binary) {
     });
 }
 
+async function promiseInstallAddon(driver, xpi) {
+  // This manually installs the add-on as a temporary add-on.
+  // Hopefully selenium/geckodriver will get a way to do this soon:
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=1298025
+  return driver.executeAsyncScript(
+    `
+    let fileUtils = Components.utils.import('resource://gre/modules/FileUtils.jsm');
+    let FileUtils = fileUtils.FileUtils;
+    let callback = arguments[arguments.length - 1];
+    Components.utils.import('resource://gre/modules/AddonManager.jsm');
+
+    let listener = {
+      onInstallEnded: function(install, addon) {
+        callback([addon.id, 0]);
+      },
+
+      onInstallFailed: function(install) {
+        callback([null, install.error]);
+      },
+
+      onInstalled: function(addon) {
+        AddonManager.removeAddonListener(listener);
+        callback([addon.id, 0]);
+      }
+    };
+
+    let file = new FileUtils.File(arguments[0]);
+
+    AddonManager.addAddonListener(listener);
+    AddonManager.installTemporaryAddon(file).catch(error => {
+      Components.utils.reportError(error); callback([null, error])
+    });`,
+    xpi)
+    .then(result => {
+      if (!result[0] && result[1]) {
+        return driver.quit().then(() => {
+          // eslint-disable-next-line no-console
+          console.error(result);
+          throw new Error('Failed to install add-on');
+        });
+      }
+
+      return driver;
+    });
+}
+
 module.exports.promiseSetupDriver = (xpi, binary) => {
   const options = new firefox.Options();
   Object.keys(FIREFOX_PREFERENCES).forEach(key => {
     options.setPreference(key, FIREFOX_PREFERENCES[key]);
   });
+  options.setBinary(await promiseActualBinary(binary))
 
-  const builder = new webdriver.Builder()
+  const driver = new webdriver.Builder()
     .forBrowser('firefox')
-    .setFirefoxOptions(options);
+    .setFirefoxOptions(options)
+    .build();
 
-  return promiseActualBinary(binary)
-    .then(binaryLocation => options.setBinary(binaryLocation))
-    .then(() => builder.build())
-    .then(driver => {
-      driver.setContext(Context.CHROME);
-
-      // This manually installs the add-on as a temporary add-on.
-      // Hopefully selenium/geckodriver will get a way to do this soon:
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=1298025
-      return driver.executeAsyncScript(
-        `
-        let fileUtils = Components.utils.import('resource://gre/modules/FileUtils.jsm');
-        let FileUtils = fileUtils.FileUtils;
-        let callback = arguments[arguments.length - 1];
-        Components.utils.import('resource://gre/modules/AddonManager.jsm');
-
-        let listener = {
-          onInstallEnded: function(install, addon) {
-            callback([addon.id, 0]);
-          },
-
-          onInstallFailed: function(install) {
-            callback([null, install.error]);
-          },
-
-          onInstalled: function(addon) {
-            AddonManager.removeAddonListener(listener);
-            callback([addon.id, 0]);
-          }
-        };
-
-        let file = new FileUtils.File(arguments[0]);
-
-        AddonManager.addAddonListener(listener);
-        AddonManager.installTemporaryAddon(file).catch(error => {
-          Components.utils.reportError(error); callback([null, error])
-        });`,
-        xpi)
-        .then(result => {
-          if (!result[0] && result[1]) {
-            return driver.quit().then(() => {
-              // eslint-disable-next-line no-console
-              console.error(result);
-              throw new Error('Failed to install add-on');
-            });
-          }
-
-          return driver;
-        });
-    });
+  driver.setContext(firefox.Context.CHROME);
+  return promiseInstallAddon(driver, xpi);
 };
